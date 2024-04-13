2d752acce03209bc036a197677b31abdf479aebe
Benjamin Jaeger <venin.e@sjon.at>
2024-04-13 13:55:35 +0200

    wachs

--- jem-2d/scripts/enemy_logic.gd
+++ jem-2d/scripts/enemy_logic.gd
@@ -1,24 +1,49 @@
 class_name enemy
 extends Node
 
-@onready var animated_sprite_2d = get_parent();
+@onready var enemy_body = $"../.."
+@onready var animated_sprite_2d = $".."
 
 # Speed at which the sprite will move
 var speed = 100
+var xdirection = -1
+var input_vector = Vector2()
+
+var detectionRange : Area2D
+signal closest_target
 
 func _ready():
 	# Get the Timer node and connect its timeout signal to this script
 	$"../../Timer".start()
+	enemy_body.scale.x = enemy_body.scale.x * xdirection
+	detectionRange.body_entered.connect(_on_body_entered)
+	detectionRange.body_exited.connect(_on_body_exited)
 
-func _process(delta):
-	var velocity = Vector2() # The sprite's movement vector
 
-	velocity.x += 1
-	animated_sprite_2d.play() # Replace "walk" with your walking animation name
-
-	velocity = velocity.normalized() * speed
-	animated_sprite_2d.position += velocity * delta
+func _physics_process(delta):
+	# Apply the movement vector to the velocity
+	
+	# Move the character and slide along collisions
+	enemy_body.move_and_slide()
 
 
 func _on_timer_timeout():
 	animated_sprite_2d.flip_h = !animated_sprite_2d.flip_h # Flip the sprite horizontally
+	
+func _on_body_entered():
+	rushtarget(closest_target)
+	
+func _on_body_exited():
+	if closest_target != null:
+		rushb()
+	
+func rushb():
+	input_vector.x = xdirection
+	input_vector.y = 0
+	
+func rushtarget(target):
+	var target_position = target.global_position
+	var direction = (target_position - enemy_body.global_position).normalized()
+	enemy_body.velocity = direction * speed
+	# Rotate your character to face the target
+	enemy_body.rotation = atan2(direction.y, direction.x)
