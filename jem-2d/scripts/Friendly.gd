0c20af65b1f40161cb5c9aa46b3dfc2995dd94bc
Tomas Neuner <tomas@9ers.at>
2024-04-13 14:12:58 +0200

    move towards enemy

--- jem-2d/scripts/Friendly.gd
+++ jem-2d/scripts/Friendly.gd
@@ -2,23 +2,36 @@ class_name Friendly
 extends Node2D
 
 @onready var body: CharacterBody2D = get_parent()
+@onready var enemyDetection: EntityDetectionComponent = get_node("../EntityDetectionComponent")
 @onready var sprite: AnimatedSprite2D = get_node("../AnimatedSprite2D")
 
-@export var defaultVelocity: Vector2 = Vector2(100, 0)
+var defaultDirection: Vector2 = Vector2.RIGHT
+
+@export var movespeed: int = 100
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	print(body)
-	body.velocity = defaultVelocity
-	pass # Replace with function body.
+func _ready() -> void:
+    # enemyDetection.closest_target.connect(_on_closest_target)
+    pass
 
+
+#body.velocity = defaultDirection * defaultSpeed
+
+func _process(delta: float) -> void:
+    move_towards(enemyDetection.scan_for_target())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	body.move_and_slide()
-	
-	if body.velocity != Vector2.ZERO:
-		sprite.play("walk")
-	else: 
-		sprite.play("idle")
+    body.move_and_slide()
+    if body.velocity != Vector2.ZERO:
+        sprite.play("walk")
+    else:
+        sprite.play("idle")
+
+
+func move_towards(closestTarget: Node2D ) -> void:
+    var dir: Vector2 = defaultDirection
+    if closestTarget != null:
+        dir = (closestTarget.position - body.position).normalized()
+    body.velocity = dir * movespeed
---
+++ jem-2d/scripts/components/EntityDetectionComponent.gd
@@ -0,0 +1,43 @@
+class_name EntityDetectionComponent
+extends Node2D
+
+@onready var body: CharacterBody2D = get_parent()
+@onready var detectionRange: Area2D = $Area2D
+
+@export_enum("Enemy", "Friendly") var detectionTarget: String
+
+var nearbyEntities: Array[Node2D] = []
+var closestEntity: Node2D         = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	detectionRange.body_entered.connect(_on_body_entered)
+	detectionRange.body_exited.connect(_on_body_exited)
+
+func scan_for_target() -> Node2D:
+	if nearbyEntities.size() != 0:
+		closestEntity = nearbyEntities[0]
+		for entity in nearbyEntities:
+			if body.position.distance_to(entity.position) < body.position.distance_to(closestEntity.position):
+				closestEntity = entity
+		print(closestEntity)
+		 #todo remove on kill
+	else:
+		closestEntity = null
+	return closestEntity
+
+
+func _on_body_entered(entity: Node2D) -> void:
+	print(entity, " entered")
+	if not entity.has_node(detectionTarget):
+		return
+	nearbyEntities.append(entity)
+
+
+func _on_body_exited(entity: Node2D) -> void:
+	print(entity, " exited")
+
+	if not entity.has_node(detectionTarget):
+		return
+	nearbyEntities.remove_at(nearbyEntities.find(entity))
