6dac6bf65351af49c736e944e4dae1143ceee0ef
Tomas Neuner <tomas@9ers.at>
2024-04-13 17:29:33 +0200

    friendlies on click, enemy on timer, animations state machine

--- jem-2d/scripts/Friendly.gd
+++ jem-2d/scripts/Friendly.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var enemyDetection: EntityDetectionComponent = get_node("../EntityDetectionComponent")
 @onready var sprite: AnimatedSprite2D = get_node("../AnimatedSprite2D")
 @onready var attackRangeGizmo: Line2D = $"../AttackRangeGizmo"
-
+@onready var animState: AnimationStateComponent = $"../AnimationStateComponent"
 
 var defaultDirection: Vector2 = Vector2.RIGHT
 var closestTarget: Node2D     = null
@@ -14,24 +14,42 @@ var closestTarget: Node2D     = null
 @export var attackRange: int = 300
 
 
+
 func _ready() -> void:
 	attackRangeGizmo.points[1].x = attackRange
+
 
 func _process(delta: float) -> void:
 	closestTarget = enemyDetection.scan_for_target()
 
+
 func _physics_process(_delta: float) -> void:
 
+	# if target is found
 	if closestTarget != null:
 		(closestTarget.position - body.position).normalized() * movespeed
 		if body.position.distance_to(closestTarget.position) <= attackRange:
 			body.velocity = Vector2.ZERO
-			sprite.play("attack")
+			animState.currentState = AnimationStateComponent.AnimationState.ATTACK
 	else:
-		
-		body.velocity = Vector2.RIGHT * movespeed
+		body.velocity = Vector2.RIGHT * movespeed if body.position.x < 750 else Vector2.ZERO
 	body.move_and_slide()
-	
+
 	if body.velocity != Vector2.ZERO:
-		sprite.play("walk")
+		animState.currentState = AnimationStateComponent.AnimationState.WALK
+	elif animState.currentState != AnimationStateComponent.AnimationState.ATTACK:
+		animState.currentState = AnimationStateComponent.AnimationState.IDLE
+
+	match animState.currentState:
+		AnimationStateComponent.AnimationState.WALK:
+			sprite.play("walk")
+		AnimationStateComponent.AnimationState.ATTACK:
+			sprite.play("attack")
+		AnimationStateComponent.AnimationState.IDLE:
+			sprite.play("idle")
+		AnimationStateComponent.AnimationState.DEATH:
+			pass
+		AnimationStateComponent.AnimationState.SPAWN:
+			pass		
+
 
--- jem-2d/scripts/spawn_manager.gd
+++ jem-2d/scripts/spawn_manager.gd
@@ -3,18 +3,29 @@ extends Node2D
 signal entity_spawned
 @export var friendlySpawnPos: Vector2
 @export var enemySpawnPos: Vector2
-@export var debugEntity: PackedScene
 
+@export var friendlyEntity: PackedScene
+@export var enemyEntity: PackedScene
+
+
+@export var enemySpawnPosX: int = 1200
+@onready var timer: Timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
-func _ready(): 
-	#_on_spawn_entity(debugEntity)
+func _ready():
+	timer.set_wait_time(2)
+	timer.timeout.connect(_on_timeout)
+	timer.start()
 	pass
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("click"):
+		spawn_entity(friendlyEntity, get_viewport().get_mouse_position())
+		
 
 #todo connect signal to _on_spawn_entity
 
-func _on_spawn_entity(entity: PackedScene, spawnPos = null) -> void:
+func spawn_entity(entity: PackedScene, spawnPos = null) -> void:
 	var ent: Node2D = entity.instantiate()
 
 	if ent.has_node("Enemy"):
@@ -30,5 +41,9 @@ func _on_spawn_entity(entity: PackedScene, spawnPos = null) -> void:
 	else:
 		print("nothing")
 		return
-
-	#ent.global_position = spawnPos
+		
+func _on_timeout():
+	var enemySpawnPosY = randi_range(0, get_viewport_rect().size.y)
+	spawn_entity(enemyEntity, Vector2(enemySpawnPosX, enemySpawnPosY))
+	timer.start()
+	
