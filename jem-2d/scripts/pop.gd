8d71315256ca6951fe7c47116f35a028fadf195c
Tomas Neuner <tomas@9ers.at>
2024-04-13 10:40:28 +0200

    grass tile map

--- jem-2d/scripts/pop.gd
+++ jem-2d/scripts/pop.gd
@@ -3,17 +3,8 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
-		sprite.set_texture(load("res://assets/img/popcat/pop_1.png"))
+		sprite.set_texture(load("res://assets/img/popcat/pop_1.png") as Texture2D)
 	if Input.is_action_just_released("click"):
-		sprite.set_texture(load("res://assets/img/popcat/pop_0.png"))
+		sprite.set_texture(load("res://assets/img/popcat/pop_0.png") as Texture2D)
