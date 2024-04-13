e13efdf3a860401c67510b65fc821b58680679f2
Tomas Neuner <tomas@9ers.at>
2024-04-13 17:29:33 +0200

    healthbar, damage, spawn on click

--- jem-2d/scripts/Friendly.gd
+++ jem-2d/scripts/Friendly.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var enemyDetection: EntityDetectionComponent = get_node("../EntityDetectionComponent")
 @onready var sprite: AnimatedSprite2D = get_node("../AnimatedSprite2D")
 @onready var attackRangeGizmo: Line2D = $"../AttackRangeGizmo"
-
+@onready var animState: AnimationStateComponent = $"../AnimationStateComponent"
 
 var defaultDirection: Vector2 = Vector2.RIGHT
 var closestTarget: Node2D     = null
@@ -15,30 +15,47 @@ var closestTarget: Node2D     = null
 @export var attack_damage: int = 1
 
 
+
 func _ready() -> void:
 	attackRangeGizmo.points[1].x = attackRange
 	sprite.animation_finished.connect(_on_animation_finished)
 
+
 func _process(delta: float) -> void:
 	closestTarget = enemyDetection.scan_for_target()
 
+
 func _physics_process(_delta: float) -> void:
 
+	# if target is found
 	if closestTarget != null:
 		(closestTarget.position - body.position).normalized() * movespeed
 		if body.position.distance_to(closestTarget.position) <= attackRange:
 			attack(closestTarget)
 	else:
-		
-		body.velocity = Vector2.RIGHT * movespeed
+		body.velocity = Vector2.RIGHT * movespeed if body.position.x < 750 else Vector2.ZERO
 	body.move_and_slide()
-	
+
 	if body.velocity != Vector2.ZERO:
-		sprite.play("walk")
+		animState.currentState = AnimationStateComponent.AnimationState.WALK
+	elif animState.currentState != AnimationStateComponent.AnimationState.ATTACK:
+		animState.currentState = AnimationStateComponent.AnimationState.IDLE
+
+	match animState.currentState:
+		AnimationStateComponent.AnimationState.WALK:
+			sprite.play("walk")
+		AnimationStateComponent.AnimationState.ATTACK:
+			sprite.play("attack")
+		AnimationStateComponent.AnimationState.IDLE:
+			sprite.play("idle")
+		AnimationStateComponent.AnimationState.DEATH:
+			pass
+		AnimationStateComponent.AnimationState.SPAWN:
+			pass
 		
 func attack(target):
 	body.velocity = Vector2.ZERO
-	sprite.play("attack")
+	animState.currentState = AnimationStateComponent.AnimationState.ATTACK
 
 func _on_animation_finished():
 	print(sprite.animation)
