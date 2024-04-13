d5de492b3e17e734010673ba2cb23972025b4902
Tomas Neuner <tomas@9ers.at>
2024-04-13 13:09:49 +0200

    friendly movement

--- jem-2d/scripts/Friendly.gd
+++ jem-2d/scripts/Friendly.gd
@@ -1,11 +1,24 @@
-class_name Friendly extends Node2D
+class_name Friendly
+extends Node2D
+
+@onready var body: CharacterBody2D = get_parent()
+@onready var sprite: AnimatedSprite2D = get_node("../AnimatedSprite2D")
+
+@export var defaultVelocity: Vector2 = Vector2(100, 0)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print(body)
+	body.velocity = defaultVelocity
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(_delta: float) -> void:
+	body.move_and_slide()
+	
+	if body.velocity != Vector2.ZERO:
+		sprite.play("walk")
+	else: 
+		sprite.play("idle")
--- jem-2d/scripts/spawn_manager.gd
+++ jem-2d/scripts/spawn_manager.gd
@@ -1,28 +1,34 @@
 extends Node2D
 
 signal entity_spawned
-@export var defaultSpawnPos: Vector2
+@export var friendlySpawnPos: Vector2
+@export var enemySpawnPos: Vector2
 @export var debugEntity: PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-    _on_spawn_entity(debugEntity)
+func _ready(): 
+	#_on_spawn_entity(debugEntity)
+	pass
 
 
 #todo connect signal to _on_spawn_entity
 
-func _on_spawn_entity(entity: PackedScene, spawnPos: Vector2 = defaultSpawnPos) -> void:
-    var ent: Node2D = entity.instantiate()
+func _on_spawn_entity(entity: PackedScene, spawnPos = null) -> void:
+	var ent: Node2D = entity.instantiate()
 
-    if ent.has_node("Enemy"):
-        $Enemies.add_child(ent)
-        print("enemy")
-    elif ent.has_node("Friendly"):
-        $Friendlies.add_child(ent)
-        print("friendly")
-    else:
-        print("nothing")
-        return
+	if ent.has_node("Enemy"):
+		$Enemies.add_child(ent)
+		print("enemy")
+		ent.position = spawnPos if spawnPos else enemySpawnPos
 
-    ent.global_position = spawnPos
+	elif ent.has_node("Friendly"):
+		$Friendlies.add_child(ent)
+		print("friendly")
+		ent.position = spawnPos if spawnPos else friendlySpawnPos
+
+	else:
+		print("nothing")
+		return
+
+	#ent.global_position = spawnPos
