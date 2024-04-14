2a34cde8f7857f2ac98582c2cd0ae567ae2010bc
Tomas Neuner <tomas@9ers.at>
2024-04-14 21:36:27 +0200

    audio title screen img

--- jem-2d/scripts/spawn_manager.gd
+++ jem-2d/scripts/spawn_manager.gd
@@ -46,14 +46,12 @@ func spawn_entity(entity: PackedScene, spawnPos: Vector2, elementalType = null):
 		ent.get_node("ElementalAffinityComponent").local_element = elementalType if elementalType != null else randi_range(0, 4) # randomizes element
 		ent.scale *= custom_random(0.7,3) + flatSizeIncrease
 		$Enemies.add_child(ent)
-		print("enemy")
 
 	elif ent.is_in_group("Friendly"):
 		if resources.spend_resource(ent.get_node("StatBlockComponent").spawn_cost):
 			ent.position = spawnPos
 			ent.get_node("ElementalAffinityComponent").local_element = elementalType if elementalType != null else randi_range(0, 4) # randomizes element
 			$Friendlies.add_child(ent)
-			print("friendly")
 	else:
 		print("nothing")
 
